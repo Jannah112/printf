3a7609563510a3d9a0a5e78902a97fdc5e4eb68a
Jannah112 <alaaosman1635@gmail.com>
2023-11-13 09:48:41 -0800

    c

--- main.h
+++ main.h
@@ -6,9 +6,13 @@
 #include <stdarg.h>
 #include <string.h>
 #include <stdlib.h>
+#include <limits.h>
 
 int _printf(const char *format, ...);
 int put_char(char c);
 int put_str(char *s);
+int counter(int number);
+void _putchar(char c);
+void print_number(int n);
 
 #endif
